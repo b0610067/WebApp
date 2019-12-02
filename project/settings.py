fe215c0fc7a5c041651c06a0774c96247055b6b1
Kelly C <b0610067@yahoo.com.tw>
2019-12-02 16:11:05 +0000

    settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ti+a)4kh$h=^@bti%x$jpeku&s7qf5u6d4gaoy97pjb*ec#@fq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
